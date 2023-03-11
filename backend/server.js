0012389e909cad0001613ab5c73c73d2293fb752
Shelley Chen <shelleychen318@gmail.com>
2023-03-10 22:40:29 -0500

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const courseRoutes = require("./routes/courses");
 
 const app = express();
@@ -14,8 +15,16 @@ app.use((req, res, next) => {
 });
 
 // mount router module onto main app
-app.use('/api/courses', courseRoutes)
+app.use("/api/courses", courseRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to mongodb and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
