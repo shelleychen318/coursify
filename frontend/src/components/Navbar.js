6402d55e9f09c552ac4311fcb80f240c719b9dc9
Shelley Chen <shelleychen318@gmail.com>
2023-03-13 17:11:35 -0400

    created react app and added styles to navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNavbar = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 10px 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledLink = styled(Link)`
+  color: var(--primary);
+  text-decoration: none;
+`;
+
+const Navbar = () => {
+  return (
+    <StyledNavbar>
+      <StyledLink to="/">
+        <h1>coursify</h1>
+      </StyledLink>
+    </StyledNavbar>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Your Courses</h2>
+    </div>
+  );
+};
+
+export default Home;
