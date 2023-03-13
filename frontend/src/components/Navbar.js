26fcf3e41292a30286993e224d04fab0ea968c0f
Shelley Chen <shelleychen318@gmail.com>
2023-03-13 17:19:06 -0400

    switched from px to rem for styles

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const StyledNavbar = styled.div`
   max-width: 1400px;
   margin: 0 auto;
-  padding: 10px 50px;
+  padding: 0.5rem 3rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
