df9cea1c2f4250db61b45ccff677fb6d23bbd16c
Shelley Chen <shelleychen318@gmail.com>
2023-03-15 15:33:51 -0400

    added title text change on click

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -23,18 +23,22 @@ const Home = () => {
 
   const [form, setForm] = useState(false);
   const [buttonText, setButtonText] = useState("+ New Course");
+  const [titleText, setTitleText] = useState("My Courses");
 
   const handleClick = () => {
     setForm(!form);
     setButtonText((state) =>
       state === "+ New Course" ? "Cancel" : "+ New Course"
     );
+    setTitleText((state) =>
+      state === "My Courses" ? "Add a course" : "My Courses"
+    );
   };
 
   return (
     <div className="container">
       <div className="titleContainer">
-        <h2>My Courses</h2>
+        <h2>{titleText}</h2>
         <button
           type="button"
           onClick={handleClick}
