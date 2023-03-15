57383cf2cb2777ac57600dc2d45b7614a0d9e5ad
Shelley Chen <shelleychen318@gmail.com>
2023-03-14 23:11:17 -0400

    added styles to input field

--- frontend/src/components/CourseForm/index.js
+++ frontend/src/components/CourseForm/index.js
@@ -4,9 +4,9 @@ const CourseForm = ({ course }) => {
   return (
     <form className="formContainer">
       <h3>Add a new course</h3>
-      <label>
-        Course Code: <input type="text" />
-      </label>
+        <label>
+          Course Code: <input type="text" />
+        </label>
     </form>
   );
 };
