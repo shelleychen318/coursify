94f12a25e987ec467fefc446ecf4a362d1d84e85
Shelley Chen <shelleychen318@gmail.com>
2023-03-15 13:20:37 -0400

    changed form to be grid layout

--- frontend/src/components/CourseForm/index.js
+++ frontend/src/components/CourseForm/index.js
@@ -3,10 +3,15 @@ import "./courseForm.css";
 const CourseForm = ({ course }) => {
   return (
     <form className="formContainer">
-      <h3>Add a new course</h3>
+      {/* <h3>Add a new course</h3> */}
+      {/* <div className="gridContainer"> */}
         <label>
           Course Code: <input type="text" />
         </label>
+        <label>
+          Course Name: <input type="text" />
+        </label>
+      {/* </div> */}
     </form>
   );
 };
