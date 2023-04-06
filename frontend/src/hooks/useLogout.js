31c9f907db926647845174be25a4141e6d6c23a2
Shelley Chen <shelleychen318@gmail.com>
2023-04-06 19:52:19 -0400

    cleared global courses state on logout

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext";
+import { useCoursesContext } from "./useCoursesContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: coursesDispatch } = useCoursesContext();
 
   const logout = () => {
     // remove user from storage
@@ -9,6 +11,9 @@ export const useLogout = () => {
 
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
+
+    // clear global courses state on logout
+    coursesDispatch({ type: "SET_COURSES", paylod: null });
   };
 
   return { logout };
