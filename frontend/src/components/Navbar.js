66752cdd1a1a75487d29019f8962838984be6147
Shelley Chen <shelleychen318@gmail.com>
2023-04-06 15:28:19 -0400

    set initial auth status on react app by checking local storage for JWT

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -15,13 +17,20 @@ const Navbar = () => {
         <h1>coursify</h1>
       </Link>
       <div className={styles.navLinks}>
-        <div>
-          <button onClick={handleClick} className={styles.logoutButton}>Log out</button>
-        </div>
-        <div className={styles.links}>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+        {user && (
+          <div>
+            <span>{user.email}</span>
+            <button onClick={handleClick} className={styles.logoutButton}>
+              Log out
+            </button>
+          </div>
+        )}
+        {!user && (
+          <div className={styles.links}>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
       </div>
     </div>
   );
